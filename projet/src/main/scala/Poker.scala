d47be5176c54c11ec758f31ec5de3a13ba9f0620
Pierre-Jean Lefort <pjgame84@gmail.com>
2022-03-16 11:42:53 +0100

    première fonction

--- projet/src/main/scala/Poker.scala
+++ projet/src/main/scala/Poker.scala
@@ -23,10 +23,20 @@ object Poker:
   enum PlayingCard:
     case Group(col: Couleurs, num: Numero)
 
-  object CarteClassement extends scala.math.Ordering[PlayingCard] {
+  object CarteClassementNumero extends scala.math.Ordering[PlayingCard] {
     override def compare(card: PlayingCard, to: PlayingCard): Int = (card, to) match {
       case (PlayingCard.Group(_, x), PlayingCard.Group(_, y)) => x.ordinal - y.ordinal;
     }
+  }
+
+  object CarteClassementCouleur extends scala.math.Ordering[PlayingCard] {
+    override def compare(card: PlayingCard, to: PlayingCard): Int = (card, to) match {
+      case (PlayingCard.Group(x, _), PlayingCard.Group(y, _)) => x.ordinal - y.ordinal;
+    }
+  }
+
+  def nextCard(card: PlayingCard): PlayingCard = card match {
+    case PlayingCard.Group(a, x) => PlayingCard.Group(a, Numero.fromOrdinal(x.ordinal + 1))
   }
 
   enum MainsPossible:
@@ -41,12 +51,68 @@ object Poker:
     case QuintFlush
     case QuintFlushRoyale
 
-  enum PokerHand:
-    case Group(main: MainsPossible)
+  def getOccurence(card: PlayingCard, main: List[PlayingCard]): Int = (main, card) match {
+    case (Nil, _) => 0
+    case (PlayingCard.Group(_, x) :: z, PlayingCard.Group(_, y)) => if (x == y) 1 + getOccurence(card, z) else getOccurence(card, z)
+  }
 
-  object HandClassement extends scala.math.Ordering[PokerHand] {
-    override def compare(hand: PokerHand, to: PokerHand): Int = (hand, to) match {
-      case (PokerHand.Group(x), PokerHand.Group(y)) => x.ordinal - y.ordinal;
+  def getMultipleOccurence(main: List[PlayingCard]): List[(PlayingCard, Int)] = main match {
+    case Nil => Nil
+    case x :: z => {
+      val occur = getOccurence(x, z)
+      val next = z.filter(p => (p, x) match {
+        case (PlayingCard.Group(_, a), PlayingCard.Group(_, b)) => a != b
+      })
+      (x, occur + 1) :: getMultipleOccurence(next)
+    }
+  }
+
+  def getCouleurOccurence(card: PlayingCard, main: List[PlayingCard]): Int = (main, card) match {
+    case (Nil, _) => 0
+    case (PlayingCard.Group(x, _) :: z, PlayingCard.Group(y, _)) => if (x == y) 1 + getCouleurOccurence(card, z) else getOccurence(card, z)
+  }
+
+  def getMainsNumero(occur: List[(PlayingCard, Int)]): List[MainsPossible] = occur match {
+    case Nil => Nil
+    case (c, i) :: z => {
+      val next = getMainsNumero(z)
+      val anotherPaire = next.filter(p => p == MainsPossible.Paire).length == 1
+      val triple = z.filter((p, i) => i > 2).length == 1
+      val carre = z.filter((p, i) => i > 3).length == 1
+      if (carre) MainsPossible.Carre :: MainsPossible.Brelan :: MainsPossible.Paire :: next else
+        if (anotherPaire && triple) MainsPossible.Full :: MainsPossible.DeuxPaire :: MainsPossible.Paire :: next else
+          if (anotherPaire) MainsPossible.DeuxPaire :: MainsPossible.Paire :: next else
+            if (triple) MainsPossible.Brelan :: MainsPossible.Paire :: next else
+              if (i > 1) MainsPossible.Paire :: next else next
+    }
+  }
+
+  def getQuinte(main: List[PlayingCard]): List[MainsPossible] = {
+    val quinte = isQuinte(main)
+    val couleur = isCouleur(main)
+
+    if (couleur && quinte) MainsPossible.Couleur :: MainsPossible.QuintFlush :: Nil else
+      if (quinte) MainsPossible.Quinte :: Nil else
+        if (couleur) MainsPossible.Couleur :: Nil else Nil
+  }
+
+  def isQuinte(main: List[PlayingCard]): Boolean = main match {
+    case Nil => false
+    case x :: y :: z => if (nextCard(x) == y) isQuinte(main) else false
+  }
+
+  def isCouleur(main: List[PlayingCard]): Boolean = main match {
+    case Nil => false
+    case PlayingCard.Group(x, _) :: PlayingCard.Group(y, _) :: z => if (x == z) isCouleur(z) else false
+  }
+
+  def getMains(main: List[PlayingCard]): List[MainsPossible] = MainsPossible.PlusHaute :: getMainsNumero(getMultipleOccurence(main)).concat(getQuinte(main))
+
+  def getBestMain(main: List[PlayingCard]): MainsPossible = getMains(main).sorted(HandClassement).head
+
+  object HandClassement extends scala.math.Ordering[MainsPossible] {
+    override def compare(hand: MainsPossible, to: MainsPossible): Int = (hand, to) match {
+      case (x, y) => y.ordinal - x.ordinal;
     }
   }
 
--- projet/src/test/scala/PokerTest.scala
+++ projet/src/test/scala/PokerTest.scala
@@ -14,4 +14,51 @@ class PokerTest extends AnyFlatSpec {
     HandClassement.compare(PokerHand.Group(MainsPossible.Full), PokerHand.Group(MainsPossible.PlusHaute)) should be > 0
     HandClassement.compare(PokerHand.Group(MainsPossible.PlusHaute), PokerHand.Group(MainsPossible.PlusHaute)) shouldEqual  0
   }
+
+  "getOccurence" should "be defined" in {
+    val maMain1 = PlayingCard.Group(Couleurs.Coeur, Numero.Deux) :: PlayingCard.Group(Couleurs.Coeur, Numero.As) :: PlayingCard.Group(Couleurs.Coeur, Numero.Trois) :: Nil
+    getOccurence(PlayingCard.Group(Couleurs.Coeur, Numero.As), maMain1) shouldEqual 1
+
+    val maMain2 = PlayingCard.Group(Couleurs.Coeur, Numero.Deux) :: PlayingCard.Group(Couleurs.Coeur, Numero.Trois) :: Nil
+    getOccurence(PlayingCard.Group(Couleurs.Coeur, Numero.As), maMain2) shouldEqual 0
+
+    val maMain3 = PlayingCard.Group(Couleurs.Coeur, Numero.Deux) :: PlayingCard.Group(Couleurs.Coeur, Numero.As) :: PlayingCard.Group(Couleurs.Coeur, Numero.Trois) :: PlayingCard.Group(Couleurs.Coeur, Numero.As) :: PlayingCard.Group(Couleurs.Coeur, Numero.Dix) :: PlayingCard.Group(Couleurs.Coeur, Numero.As) :: Nil
+    getOccurence(PlayingCard.Group(Couleurs.Coeur, Numero.As), maMain3) shouldEqual 3
+  }
+
+  "getCouleurOccurence" should "be defined" in {
+    val maMain1 = PlayingCard.Group(Couleurs.Coeur, Numero.Deux) :: PlayingCard.Group(Couleurs.Coeur, Numero.As) :: PlayingCard.Group(Couleurs.Coeur, Numero.Trois) :: Nil
+    getCouleurOccurence(PlayingCard.Group(Couleurs.Coeur, Numero.As), maMain1) shouldEqual 3
+
+    val maMain2 = PlayingCard.Group(Couleurs.Coeur, Numero.Deux) :: PlayingCard.Group(Couleurs.Carreau, Numero.Trois) :: Nil
+    getCouleurOccurence(PlayingCard.Group(Couleurs.Coeur, Numero.As), maMain2) shouldEqual 1
+
+    val maMain3 = PlayingCard.Group(Couleurs.Coeur, Numero.Deux) :: PlayingCard.Group(Couleurs.Trefle, Numero.As) :: PlayingCard.Group(Couleurs.Pique, Numero.Trois) :: PlayingCard.Group(Couleurs.Coeur, Numero.As) :: PlayingCard.Group(Couleurs.Carreau, Numero.Dix) :: PlayingCard.Group(Couleurs.Coeur, Numero.As) :: Nil
+    getCouleurOccurence(PlayingCard.Group(Couleurs.Coeur, Numero.As), maMain3) shouldEqual 3
+  }
+
+  "getMultipleOccurence" should "be defined" in {
+    val maMain1 = PlayingCard.Group(Couleurs.Coeur, Numero.Deux) :: PlayingCard.Group(Couleurs.Coeur, Numero.As) :: PlayingCard.Group(Couleurs.Coeur, Numero.Deux) :: PlayingCard.Group(Couleurs.Coeur, Numero.As) :: PlayingCard.Group(Couleurs.Coeur, Numero.As) :: Nil
+    val maMain12 = (PlayingCard.Group(Couleurs.Coeur, Numero.Deux), 2) :: (PlayingCard.Group(Couleurs.Coeur, Numero.As), 3) :: Nil
+    getMultipleOccurence(maMain1) shouldBe maMain12
+  }
+
+  "getMainsNumero" should "be defined" in {
+    val maMain1 = (PlayingCard.Group(Couleurs.Coeur, Numero.Deux), 3) :: (PlayingCard.Group(Couleurs.Coeur, Numero.As), 2) :: Nil
+    val maMain12 = MainsPossible.Full :: Nil
+    getMainsNumero(maMain1) shouldBe maMain12
+  }
+
+  "getMains" should "be defined" in {
+    val maMain1 = PlayingCard.Group(Couleurs.Coeur, Numero.Deux) :: PlayingCard.Group(Couleurs.Coeur, Numero.As) :: PlayingCard.Group(Couleurs.Coeur, Numero.Deux) :: PlayingCard.Group(Couleurs.Coeur, Numero.As) :: PlayingCard.Group(Couleurs.Coeur, Numero.As) :: Nil
+    val mains = MainsPossible.PlusHaute :: MainsPossible.Full :: MainsPossible.DeuxPaire :: MainsPossible.Paire :: MainsPossible.Paire :: Nil
+    getMains(maMain1) shouldBe mains
+  }
+
+  "getBestMains" should "be defined" in {
+    val maMain1 = PlayingCard.Group(Couleurs.Coeur, Numero.Deux) :: PlayingCard.Group(Couleurs.Coeur, Numero.As) :: PlayingCard.Group(Couleurs.Coeur, Numero.Deux) :: PlayingCard.Group(Couleurs.Coeur, Numero.As) :: PlayingCard.Group(Couleurs.Coeur, Numero.As) :: Nil
+    println(getMains(maMain1).sorted(HandClassement))
+    getBestMain(maMain1) shouldBe MainsPossible.Full
+  }
+
 }
