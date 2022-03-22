122e2ad2c3b2dc6c6417d188ef241b34cefe162d
Pierre-Jean Lefort <pjgame84@gmail.com>
2022-03-22 18:13:20 +0100

    Changes by thomasmauran

--- projet/src/test/scala/PokerTest.scala
+++ projet/src/test/scala/PokerTest.scala
@@ -3,16 +3,18 @@ import org.scalatest.matchers.should.Matchers.{an, shouldBe, be, should, shouldE
 import Poker.*
 
 class PokerTest extends AnyFlatSpec {
+  /*
   "CarteClassement" should "be defined" in {
     CarteClassement.compare(PlayingCard.Group(Couleurs.Coeur, Numero.As), PlayingCard.Group(Couleurs.Coeur, Numero.Deux)) should be > 0
     CarteClassement.compare(PlayingCard.Group(Couleurs.Coeur, Numero.Deux), PlayingCard.Group(Couleurs.Coeur, Numero.As)) should be < 0
     CarteClassement.compare(PlayingCard.Group(Couleurs.Coeur, Numero.As), PlayingCard.Group(Couleurs.Coeur, Numero.As)) shouldEqual 0
   }
+  */
 
   "HandClassement" should "be defined" in {
-    HandClassement.compare(PokerHand.Group(MainsPossible.PlusHaute), PokerHand.Group(MainsPossible.Full)) should be < 0
-    HandClassement.compare(PokerHand.Group(MainsPossible.Full), PokerHand.Group(MainsPossible.PlusHaute)) should be > 0
-    HandClassement.compare(PokerHand.Group(MainsPossible.PlusHaute), PokerHand.Group(MainsPossible.PlusHaute)) shouldEqual  0
+    HandClassement.compare(MainsPossible.PlusHaute, MainsPossible.Full) should be > 0
+    HandClassement.compare(MainsPossible.Full, MainsPossible.PlusHaute) should be < 0
+    HandClassement.compare(MainsPossible.PlusHaute, MainsPossible.PlusHaute) shouldEqual  0
   }
 
   "getOccurence" should "be defined" in {
